9f34921ddc3fc43aed00ae546b4730ff29802bce
Amit Shukla <amit.stp2001@gmail.com>
2025-01-08 21:39:52 +0000

    addedd palo-alto-fw and gcp fw for mgmt nw

--- backbone/global/main.tf
+++ backbone/global/main.tf
@@ -217,6 +217,18 @@ resource "google_compute_subnetwork" "vpc_glbl_p_mgmt_subnet_asso2" {
 }
 
 
+resource "google_compute_firewall" "allow_ssh_iap_https_vpc_glbl_p_mgmt" {
+  name    = "allow-ssh-iap-https-vpc-glbl-p-mgmt"
+  network = google_compute_network.vpc_glbl_p_mgmt.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "443"]
+  }
+  target_tags = ["palo-alto-fw"]
+  source_ranges = ["0.0.0.0/0", "35.235.240.0/20"]
+}
+
 
 ####################End of Hub Project and Global VPCs#################################
 
@@ -273,3 +285,62 @@ resource "google_project_service" "host_platform_prj_cloudresourcemanager" {
 }
 
 #############################End of Spoke Projects##############################################
+
+############################# Firewall configs start ##############################################
+
+resource "google_compute_instance" "palo_alto_vmseries_fw" {
+  name         = "palo-alto-vmseries-fw"
+  machine_type = "n2-standard-4"
+  zone         = "europe-west3-b"
+  project = "host-hub-prj-18200947911"
+
+  boot_disk {
+    initialize_params {
+      image = "projects/mpi-paloaltonetworksgcp-public/global/images/vmseries-flex-bundle2-1123h3"
+      size = 60
+      type = "pd-ssd"
+    }
+  }
+
+  network_interface {
+    #mgmt
+    network       = google_compute_network.vpc_glbl_p_mgmt.name
+    subnetwork = google_compute_subnetwork.vpc_glbl_p_mgmt_subnet_euwe3.name
+    access_config {
+    }
+  }
+
+  network_interface {
+    #untrust
+    network       = google_compute_network.vpc_glbl_p_untrust.name
+    subnetwork = google_compute_subnetwork.vpc_glbl_p_untrust_subnet_euwe3.name
+    access_config {
+    }
+  }
+
+    network_interface {
+    #hub1
+    network = "vpc-euwe3-p-hub01"
+    subnetwork = "vpc-euwe3-p-hub01-subnet"
+  }
+    network_interface {
+    #hub2
+    network = "vpc-euwe3-p-hub02"
+    subnetwork = "vpc-euwe3-p-hub02-subnet"
+  }
+
+##commenting this because this vm type only supports 4 nw interfaces i.e. 0,1,2,3
+//  network_interface {
+//  #transit
+//  network       = google_compute_network.vpc_glbl_p_transit.name
+//  subnetwork = google_compute_subnetwork.vpc_glbl_p_transit_subnet_euwe3.name
+//  }
+//
+  metadata = {
+    ssh-keys = "admin:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCh4TscUxwqcvTpYWV01FaygayFZypp3IK/eUK7XIxuz72xyRdMdFJ/IOV98a7WtZHLVt+wnbi/cfRFmGsYcMBrMb1GrlThv7/y5jxPTapsOrcyRwpSmZ7ugkY0cGlE++LZRt5S/vjgw9dChvwT1rP3ljTC3a8V095Wl6609meJ5OCcr7eHz/boBbS2ZAfN1ij55z55kYHB3lp1z7ZCcPr3MdLrKbIRPQ+KbiPKxC0mSekTgmRRE+o879jfTDXoQD00zti1TwMqbLtySveQ4inXL5qJ8Mxqjl9BOOycD3+/RVzjLas/TAQ/cpnDA3pWr74tqISMNzGGLmTxrSQE1FTd rsa-key-20250108 admin"
+  }
+
+  tags = ["palo-alto-fw"]
+
+}
+############################# Firewall configs end ##############################################
